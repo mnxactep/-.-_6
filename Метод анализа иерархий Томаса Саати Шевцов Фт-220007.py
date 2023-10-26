b99139065e3889881e02e26803b787d3f9870e5c
mnxactep <MNXACTEP2@gmail.com>
2023-10-26 16:43:18 +0500

    Переделал методику

--- Метод анализа иерархий Томаса Саати Шевцов Фт-220007.py
+++ Метод анализа иерархий Томаса Саати Шевцов Фт-220007.py
@@ -1,61 +1,40 @@
-def get_input():
-    # Запрос ввода количества критериев
-    while True:
-        try:
-            n = int(input("Введите количество критериев: "))
-            if n <= 0:
-                raise ValueError
-            break
-        except ValueError:
-            print("Ошибка ввода. Введите положительное целое число.")
+import numpy as np
 
-    # Запрос ввода данных попарного сравнения критериев
-    matrix = []
-    for i in range(n):
-        row = []
-        for j in range(n):
-            if i == j:
-                row.append(1)
-            else:
-                while True:
-                    try:
-                        value = float(input(f"Введите коэффициент сравнения между критерием {i+1} и критерием {j+1}: "))
-                        if value <= 0:
-                            raise ValueError
-                        break
-                    except ValueError:
-                        print("Ошибка ввода. Введите положительное число.")
-                row.append(value)
-        matrix.append(row)
+while True:
+    try:
+        num_criteria = int(input("Введите кол-во критериев: "))
+        break
+    except ValueError:
+        print('Неверное значение')
 
-    return matrix
+s_matrix = np.zeros((num_criteria, num_criteria))  # Создаём матрицу нулей
 
+# Заполняем матрицу коэффициентами сравнений
+a = 1  # счётчик для пропуска уже заполненных значений
+for i in range(a, num_criteria + 1):
+    for j in range(a + 1, num_criteria + 1):
+        while True:
+            try:
+                # Заполняем каждый элемент строки матрицы
+                s_matrix[i - 1][j - 1] = round(float(input('Введите сравнение К{0}-К{1}:'.format(i, j))), 3)
+                break
+            except ValueError:
+                print('Неверное значение')
 
-def calculate_weights(matrix):
-    # Вычисление весовых коэффициентов
-    n = len(matrix)
-    weights = []
-    for i in range(n):
-        product = 1
-        for j in range(n):
-            product *= matrix[i][j] ** (1/n)
-        weights.append(product)
+        # Заполняем ячейки для обратного отношения (К1-К2 -> К2-К1)
+        s_matrix[j - 1][i - 1] = round(1 / s_matrix[i - 1][j - 1], 2)
 
-    # Нормализация весовых коэффициентов
-    sum_weights = sum(weights)
-    normalized_weights = [weight / sum_weights for weight in weights]
+    a += 1
 
-    return normalized_weights
+# Создаём список сумм строки
+comp_list = [round(sum(row), 2) for row in s_matrix]
+out_list = [round(n / sum(comp_list), 2) for n in comp_list]
 
+if sum(out_list) != 1.0:
+    index = out_list.index(max(out_list))
+    k = sum(out_list) - 1.0
+    out_list[index] -= round(k, 2)
 
-def main():
-    matrix = get_input()
-    weights = calculate_weights(matrix)
-
-    # Вывод весовых коэффициентов
-    for weight in weights:
-        print(f"{weight:.2f}")
-
-
-if __name__ == "__main__":
-    main()
+print('Весовые коэффициенты')
+for weight in out_list:
+    print(weight, end=' ')
